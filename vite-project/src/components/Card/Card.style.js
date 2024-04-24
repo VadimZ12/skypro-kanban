fd0d267333fbf8dbf076c8df65f1df21d33d21e6
Vadim <vadimsvu12@mail.ru>
2024-04-24 23:23:02 +0300

    Частично сделал задание

---
+++ vite-project/src/components/Card/Card.style.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { themeStyles } from "../../lib/theme";
+
+export const CardItem = styled.div`
+  padding: 5px;
+  animation-name: card-animation;
+  animation-duration: 500ms;
+  animation-timing-function: linear;
+`;
+
+export const CardWrapper = styled.div`
+  width: 220px;
+  height: 130px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: stretch;
+  padding: 15px 13px 19px;
+`;
+
+export const CardThemeText = styled.p`
+  font-size: 10px;
+  font-weight: 600;
+  line-height: 10px;
+`;
+
+export const CardTheme = styled.div`
+  width: auto;
+  height: 20px;
+  padding: 5px 14px;
+  border-radius: 18px;
+  background-color: ${({ $themeColor }) =>
+    themeStyles[$themeColor]?.backgroundColor || "#b4fdd1"};
+
+  ${CardThemeText} {
+    color: ${({ $themeColor }) => themeStyles[$themeColor]?.color || "#06b16e"};
+  }
+`;
